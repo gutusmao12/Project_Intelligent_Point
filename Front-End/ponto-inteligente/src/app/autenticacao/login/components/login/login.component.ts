f74d928a5e49d1a862d532c641471217ddb2a1d2
Gustavo Vilela <gutusmao12@hotmail.com>
2022-07-22 18:20:45 -0300

    Iniciando service do login

--- Front-End/ponto-inteligente/src/app/autenticacao/login/components/login/login.component.ts
+++ Front-End/ponto-inteligente/src/app/autenticacao/login/components/login/login.component.ts
@@ -4,21 +4,22 @@ import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { Login } from '../../models';
+import { LoginService } from '../..';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
   form: FormGroup;
 
   constructor(
     private fb: FormBuilder,
-    private snackBar: MatSnackBar//,
-    //private router: Router
-  ) { }
+    private snackBar: MatSnackBar,
+    private router: Router,
+    private loginService: LoginService
+  ) {}
 
   ngOnInit(): void {
     this.gerarForm();
@@ -27,19 +28,39 @@ export class LoginComponent implements OnInit {
   gerarForm() {
     this.form = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      senha: ['', [Validators.required, Validators.minLength(6)]]
+      senha: ['', [Validators.required, Validators.minLength(6)]],
     });
   }
 
   logar() {
     if (this.form.invalid) {
-      this.snackBar.open(
-        "Dados Inválidos", "Error", { duration: 5000 }
-      );
       return;
     }
     const login: Login = this.form.value;
-    alert('Email: ' + login.email + ', senha: ' + login.senha);
+    this.loginService.logar(login).subscribe(
+      (data) => {
+        console.log(JSON.stringify(data));
+        localStorage['token'] = data['data']['token'];
+        const usuarioData = JSON.parse(
+          atob(data['data']['token'].split('.')[1])
+        );
+        console.log(JSON.stringify(usuarioData));
+        if (usuarioData['role'] == 'ROLE_ADMIN') {
+          alert('Deve redirecionar para a página de admin');
+          this.router.navigate(['/admin']);
+        } else {
+          alert('Deve redirecionar para a página de funcionário');
+          this.router.navigate(['/funcionario']);
+        }
+      },
+      (err) => {
+        console.log(JSON.stringify(err));
+        let msg: string = 'Tente novamente em instantes.';
+        if (err['status'] == 401) {
+          msg = 'Email/senha inválido(s).';
+        }
+        this.snackBar.open(msg, "Erro", { duration: 5000 });
+      }
+    );
   }
-
 }
--- Front-End/ponto-inteligente/src/app/autenticacao/login/login.module.ts
+++ Front-End/ponto-inteligente/src/app/autenticacao/login/login.module.ts
@@ -14,6 +14,7 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { LoginComponent, LogarComponent } from './components';
+import { LoginService } from '.';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,9 @@ import { LoginComponent, LogarComponent } from './components';
     MatIconModule,
     MatSnackBarModule,
     FlexLayoutModule
+  ],
+  providers: [
+    LoginService
   ]
 })
 export class LoginModule { }
